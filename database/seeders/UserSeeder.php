f97e7adfe8684016175ad51bb0032968b1c7dd00
Linh Van Nguyen <linhvng5420@gmail.com>
2024-05-13 14:36:01 +0700

    Create Seed Table

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'username' => 'user1',
+                'email' => 'user1@example.com',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+            ],
+            [
+                'username' => 'user2',
+                'email' => 'user2@example.com',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+            ],
+            [
+                'username' => 'user3',
+                'email' => 'user3@example.com',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+            ],
+            [
+                'username' => 'user4',
+                'email' => 'user4@example.com',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+            ],
+            [
+                'username' => 'user5',
+                'email' => 'user5@example.com',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
