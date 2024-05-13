a201fc7ef060c7e6801eb1dd2e972f891299ddff
Linh Van Nguyen <linhvng5420@gmail.com>
2024-05-13 14:34:59 +0700

    Create table phone

---
+++ database/migrations/2024_05_13_065309_create_phone_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phone', function (Blueprint $table) {
+            $table->id();
+            $table->string('TenSP');
+            $table->string('HinhAnh');
+            $table->string('MauSac');
+            $table->integer('Gia');
+            $table->integer('Ram')->nullable();
+            $table->integer('Rom')->nullable();
+            $table->string('MieuTa');
+            $table->string('Hang');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('phone');
+    }
+};
