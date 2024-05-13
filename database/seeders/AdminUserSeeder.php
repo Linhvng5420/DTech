4769ef19e370b92f7572827a3698c245a1166b56
Linh Van Nguyen <linhvng5420@gmail.com>
2024-05-13 15:00:39 +0700

    Update admin seeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -18,7 +18,7 @@ class AdminUserSeeder extends Seeder
             'username' => 'admin',
             'email' => 'admin@tdc.vn',
             'password' => Hash::make('1234'),
-            'profile_image' => null,
+            'role' => 'admin'
         ]);
     }
 }
