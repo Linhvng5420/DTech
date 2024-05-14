438d6553a3da836e5f6d9ba300d3574cfc7eea6d
Linh Van Nguyen <linhvng5420@gmail.com>
2024-05-14 08:49:04 +0700

    Update Form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @stack('styles')
   <style>
     body {
       font-family: Arial, sans-serif;
@@ -36,6 +37,11 @@
       padding: 1%;
       border: 1px solid black;
       justify-items: center;
+    }
+
+    .content-yield {
+        display: grid;
+        justify-content: center;
     }
   </style>
 </head>
