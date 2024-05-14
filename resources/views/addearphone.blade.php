86360de1d5e752c7f34510c81ce79b6f29113f40
Linh Van Nguyen <linhvng5420@gmail.com>
2024-05-14 08:42:44 +0700

    Update Form

--- resources/views/addearphone.blade.php
+++ resources/views/addearphone.blade.php
@@ -1,21 +1,27 @@
 @extends('dashboard')
 
 @section('content')
-    <h1>Thêm Sản Phẩm EarPhone</h1>
+    <div style="display: grid; justify-content: center;">
+        <h1>Thêm Sản Phẩm EarPhone</h1>
 
-    @if (session('success'))
-        <div>{{ session('success') }}</div>
-    @endif
+        @if (session('success'))
+            <div>{{ session('success') }}</div>
+        @endif
 
-    <form action="{{ route('earphones.store') }}" method="POST">
-        @csrf
-        Tên Sản Phẩm: <input type="text" name="TenSP" required><br>
-        Hình Ảnh: <input type="text" name="HinhAnh"><br>
-        Màu Sắc: <input type="text" name="MauSac" required><br>
-        Giá: <input type="number" name="Gia" required><br>
-        Miêu Tả: <textarea name="MieuTa"></textarea><br>
-        Hãng: <input type="text" name="Hang" required><br>
+        <form action="{{ route('earphones.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            Tên Sản Phẩm: <input type="text" name="TenSP" required><br>
+            Màu Sắc: <input type="text" name="MauSac" required><br>
+            Giá: <input type="number" name="Gia" required><br>
+            Miêu Tả: <textarea name="MieuTa"></textarea><br>
+            Hãng: <input type="text" name="Hang" required><br>
 
-        <button type="submit">Thêm Sản Phẩm</button>
-    </form>
+            <div class="form-group">
+                <label for="HinhAnh">Hình Ảnh:</label>
+                <input type="file" id="HinhAnh" name="HinhAnh">
+            </div>
+
+            <button type="submit">Thêm Sản Phẩm</button>
+        </form>
+    </div>
 @endsection
