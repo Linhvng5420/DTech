00e079556caa82783ce8ca034b7981e654a0ef30
Linh Van Nguyen <linhvng5420@gmail.com>
2024-05-14 22:22:46 +0700

    Update Controller (Phone)

--- app/Http/Controllers/ListProductsController.php
+++ app/Http/Controllers/ListProductsController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 use App\Models\Laptop;
+use App\Models\Phone;
 
 use Illuminate\Http\Request;
 
 class ListProductsController extends Controller
 {
-    public function showLaptops() {
+    public function showProducts() {
         $laptops = Laptop::paginate(5);
-        return view('listproducts', compact('laptops'));
+//        return view('listproducts', compact('laptops'));
+
+        $phones = Phone::paginate(5);
+        return view('listproducts', compact('phones', 'laptops'));
     }
-
-
 }
