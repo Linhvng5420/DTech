3010e77d4470cf73835ab1e1b874d917dfb99095
Linh Van Nguyen <linhvng5420@gmail.com>
2024-05-15 00:37:39 +0700

    Create Model

---
+++ app/Models/Phone.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Phone extends Model
+{
+    use HasFactory;
+
+    protected $table = 'phone';
+
+    protected $fillable = [
+        'MaSP', 'TenSP', 'HinhAnh', 'MauSac', 'Gia', 'Ram', 'Rom', 'MieuTa', 'Hang'
+    ];
+}
