d8edea196eaf8a1248ef04c7bc2915435aed8c46
Linh Van Nguyen <linhvng5420@gmail.com>
2024-05-15 00:38:53 +0700

    Create Sub-Navbar For Products

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link">Phone</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">Laptop</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">Desktop</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">Mouse</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">EarPhone</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">Screen</a>
+            </li>
+        </ul>
+    </div>
+</nav>
