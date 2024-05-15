bad1b0975f87c8d54bb35dc6a28a3ec26fb97ef3
Linh Van Nguyen <linhvng5420@gmail.com>
2024-05-15 17:59:54 +0700

    Đỏi tên file

--- resources/views/navsub.blade.php
+++ resources/views/navsub.blade.php
@@ -2,22 +2,22 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link">Phone</a>
+                <a class="nav-link" href="#">Phone</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link">Laptop</a>
+                <a class="nav-link" href="#">Laptop</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link">Desktop</a>
+                <a class="nav-link" href="#">Desktop</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link">Mouse</a>
+                <a class="nav-link" href="#">Mouse</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link">EarPhone</a>
+                <a class="nav-link" href="#">EarPhone</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link">Screen</a>
+                <a class="nav-link" href="#">Screen</a>
             </li>
         </ul>
     </div>
