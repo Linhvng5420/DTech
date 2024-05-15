fc8ca9316361e6ef4a3013817934c796f5fe59a1
Linh Van Nguyen <linhvng5420@gmail.com>
2024-05-15 18:00:59 +0700

    Hiển thị sản phảm có điều kiện theo loại sản phẩm

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,26 @@
 @section('content')
     <div class="container">
         <div class="row">
-            @foreach ($phones as $phone)
-                <div class="col-md-4">
-                    <div class="card">
-                        <img src="{{ $phone->HinhAnh }}" alt="{{ $phone->TenSP }}" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $phone->TenSP }}</h5>
-                            <p class="card-text">{{ $phone->MieuTa }}</p>
-                            <p class="card-text">Giá: {{ number_format($phone->Gia) }} VND</p>
+            @foreach ($products as $product)
+                <div class="col-md-4 mt-2">
+                    <div class="card" style="padding: 5px;height: 30rem; display: grid; justify-items: center; align-items: center;">
+                        <div>
+                            <img src="{{ asset('images/' . $product->HinhAnh) }}" alt="{{ $product->TenSP }}"
+                                 class="card-img-top" style="max-width: 200px; max-height: 190px; object-fit: cover;">
                         </div>
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $product->TenSP }}</h5>
+                            <p class="card-text">{{ $product->MieuTa }}</p>
+                            <p class="card-text">Giá: {{ number_format($product->Gia) }} VND</p>
+                        </div>
+
+                        <form action="#" method="POST">
+                            @csrf
+                            <input type="hidden" name="product_id" value="{{ $product->id }}">
+                            <button type="submit" class="btn btn-primary btn-sm float-right">
+                                <i class="fas fa-plus"></i> Thêm vào giỏ hàng
+                            </button>
+                        </form>
                     </div>
                 </div>
             @endforeach
