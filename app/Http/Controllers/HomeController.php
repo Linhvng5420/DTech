4b901371ea1dab3c084ca6241497b69dbc12a0fa
Linh Van Nguyen <linhvng5420@gmail.com>
2024-05-15 18:01:42 +0700

    Update Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,12 +15,42 @@ class HomeController extends Controller
     public function index()
     {
         $phones = Phone::all();
-        $laptops = Laptop::all();
-        $desktops = Desktop::all();
-        $mice = Mouse::all();
-        $earphones = Earphone::all();
-        $screens = Screen::all();
+        return view('home', ['products' => $phones, 'type' => 'phone']);
+    }
 
-        return view('home', compact('phones', 'laptops', 'desktops', 'mice', 'earphones', 'screens'));
+    public function showPhones()
+    {
+        $phones = Phone::all();
+        return view('home', ['products' => $phones, 'type' => 'phone']);
+    }
+
+    public function showLaptops()
+    {
+        $laptops = Laptop::all();
+        return view('home', ['products' => $laptops, 'type' => 'laptop']);
+    }
+
+    public function showDesktops()
+    {
+        $desktops = Desktop::all();
+        return view('home', ['products' => $desktops, 'type' => 'desktop']);
+    }
+
+    public function showMice()
+    {
+        $mice = Mouse::all();
+        return view('home', ['products' => $mice, 'type' => 'mouse']);
+    }
+
+    public function showEarphones()
+    {
+        $earphones = Earphone::all();
+        return view('home', ['products' => $earphones, 'type' => 'earphone']);
+    }
+
+    public function showScreens()
+    {
+        $screens = Screen::all();
+        return view('home', ['products' => $screens, 'type' => 'screen']);
     }
 }
