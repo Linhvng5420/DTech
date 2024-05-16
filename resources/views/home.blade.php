30a35503d682004d9f31e739d270e3854644d07b
Linh Van Nguyen <linhvng5420@gmail.com>
2024-05-16 10:56:55 +0700

    Bắt sự kiện khi nhấn để -> trang chi tiết sp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,19 +7,24 @@
                 <div class="col-md-4 mt-2">
                     <div class="card" style="padding: 5px; height: 30rem; width: 20rem;
         display: grid; justify-items: center; align-items: center;">
-                        <div>
-                            @php
-                                $productType = strtolower(str_replace('App\\Models\\', '', get_class($product)));
-                            @endphp
-                            <img src="{{ asset('uploads/'. $productType .'/' . $product->HinhAnh) }}"
-                                 alt="{{ $product->TenSP }}"
-                                 class="card-img-top" style="max-width: 200px; max-height: 190px; object-fit: cover;">
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $product->TenSP }}</h5>
-                            <p class="card-text">{{ $product->MieuTa }}</p>
-                            <p class="card-text">Giá: {{ number_format($product->Gia) }} VND</p>
-                        </div>
+
+                        <a href="{{ route('product.view', ['productType' => strtolower(str_replace('App\\Models\\', '', get_class($product))), 'id' => $product->id]) }}"
+                           class="text-decoration-none">
+                            <div style="display: flex;justify-content: center;">
+                                @php
+                                    $productType = strtolower(str_replace('App\\Models\\', '', get_class($product)));
+                                @endphp
+                                <img src="{{ asset('uploads/'. $productType .'/' . $product->HinhAnh) }}"
+                                     alt="{{ $product->TenSP }}"
+                                     class="card-img-top"
+                                     style="max-height: 200px; object-fit: contain;">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title" style="font-weight: bold; text-align: center;">{{ mb_strtoupper(($productType).' - '.($product->TenSP))}}</h5>
+                                <p class="card-text">{{ $product->MieuTa }}</p>
+                                <p class="card-text">Giá: {{ number_format($product->Gia) }} VND</p>
+                            </div>
+                        </a>
 
                         <!--Nút Giỏ Hàng (Chưa action)-->
                         <form action="#" method="POST">
@@ -37,7 +42,6 @@
         <!--Phân Trang-->
         <div class="d-flex justify-content-center mt-4">
             {{ $products->appends(request()->query())->links('pagination::bootstrap-4') }}
-            {{--            {{ $products->links('pagination::bootstrap-4') }}--}}
         </div>
     </div>
 @endsection
